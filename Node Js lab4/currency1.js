76e55fff981ed02cd9f357655008f3a231343990
Arczisork <arczisork@gmail.com>
2025-11-14 17:46:12 +0100

    poprawki

--- Node Js lab4/currency1.js
+++ Node Js lab4/currency1.js
@@ -29,6 +29,10 @@ return roundTwoDecimals(us / canadianDollar);
 function roundTwoDecimals(amount) {
 return Math.round(amount * 100) / 100;
 }
+
+
+
+
 exports.dollarToZloty = function(dollar) {
 return roundTwoDecimals(dollar * zlotyDollar);
 }
@@ -48,49 +52,3 @@ return roundTwoDecimals(funt * zlotyFunt);
 exports.frankToZloty = function(frank) {
 return roundTwoDecimals(frank * zlotyFrank);
 }
-
-const currency = require('./currency1');
-
-/*console.log('50 dolarów kanadyjskich odpowiada następującej ilości dolarów amerykańskich:');
-
-console.log(currency.canadianToUS(50));
-
-console.log('30 dolarów amerykańskich odpowiadanastępującej ilości dolarów kanadyjskich:');
-
-console.log(currency.USToCanadian(30));
-
-*/
-
-
-console.log('50 dolarów odpowiada następującej ilości zlotych:');
-
-console.log(currency.dollarToZloty(50));
-
-console.log('50 euro odpowiada następującej ilości zlotych:');
-
-console.log(currency.euroToZloty(50));
-
-console.log('50 dolarów kanadyjskich odpowiada następującej ilości zlotych:');
-
-console.log(currency.canadianToZloty(50));
-
-console.log('50 funtów odpowiada następującej ilości zlotych:');
-
-console.log(currency.funtToZloty(50));
-
-console.log('50 franków odpowiada następującej ilości zlotych:');
-
-console.log(currency.frankToZloty(50));
-
-
-
-
-
-
-
-
-
-
-//Ścieżka dostępu używa ./ w celu wskazania,że moduł znajduje się w tym samymkatalogu, w którym jest skrypt aplikacji.
-//Użycie funkcji canadianToUS() modułu
-//Użycie funkcji USToCanadian() modułu
---
+++ Node Js lab4/module_test.js
@@ -0,0 +1,45 @@
+const currency = require('./currency1');
+
+/*console.log('50 dolarów kanadyjskich odpowiada następującej ilości dolarów amerykańskich:');
+
+console.log(currency.canadianToUS(50));
+
+console.log('30 dolarów amerykańskich odpowiadanastępującej ilości dolarów kanadyjskich:');
+
+console.log(currency.USToCanadian(30));
+
+*/
+
+
+console.log('50 dolarów odpowiada następującej ilości zlotych:');
+
+console.log(currency.dollarToZloty(50));
+
+console.log('50 euro odpowiada następującej ilości zlotych:');
+
+console.log(currency.euroToZloty(50));
+
+console.log('50 dolarów kanadyjskich odpowiada następującej ilości zlotych:');
+
+console.log(currency.canadianToZloty(50));
+
+console.log('50 funtów odpowiada następującej ilości zlotych:');
+
+console.log(currency.funtToZloty(50));
+
+console.log('50 franków odpowiada następującej ilości zlotych:');
+
+console.log(currency.frankToZloty(50));
+
+
+
+
+
+
+
+
+
+
+//Ścieżka dostępu używa ./ w celu wskazania,że moduł znajduje się w tym samymkatalogu, w którym jest skrypt aplikacji.
+//Użycie funkcji canadianToUS() modułu
+//Użycie funkcji USToCanadian() modułu
