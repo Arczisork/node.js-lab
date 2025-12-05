946499637c95bbec2148b858559d00d88b300800
Arczisork <arczisork@gmail.com>
2025-12-05 17:48:37 +0100

    lab6

---
+++ Lab6ReactDev/my/src/App.js
@@ -0,0 +1,28 @@
+
+import ToggleDetails from './ToggleDetails';
+import ScoreDisplay from './ScoreDIsplay';
+import TaskList from './TaskList';
+function App() {
+  return (
+    <div >
+      
+       
+       <ToggleDetails />
+       <ScoreDisplay score='30'/>
+      <TaskList />
+
+
+
+
+
+
+
+    </div>
+
+
+
+  );
+}
+
+export default App;
+
